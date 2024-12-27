1e40d8306dd6a53008d491d5e33b6dbae9b982f9
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2024-12-27 13:53:37 -0800

    Current Status

--- server/app.py
+++ server/app.py
@@ -9,6 +9,7 @@ from models import db, Pet
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
@@ -24,8 +25,13 @@ def index():
 
 @app.route('/demo_json')
 def demo_json():
-    pet_json = '{"id": 1, "name" : "Fido", "species" : "Dog"}'
-    return make_response(pet_json, 200)
+    pet = Pet.query.first()
+    pet_dict = {
+        'id': pet.id, 
+        'name' : pet.name, 
+        'species' : pet.species
+    }
+    return make_response(pet_dict, 200)
 
 
 if __name__ == '__main__':
